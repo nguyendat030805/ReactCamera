9d46ef4ade310f75843cdd05324c8f188197b3ca
nguyendat030805 <dat656473@gmail.com>
2025-04-18 13:49:00 +0700

    done 2

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import './components/Header/Header';
+import './components/Content/Content';
+import './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+
+    </>
+    
+
+  );
+}
+
+export default App;
